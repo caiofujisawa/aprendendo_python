923f43ddc370f6702a797799eb128611a06d47b2
caiofujisawa <118073166+caiofujisawa@users.noreply.github.com>
2022-11-12 14:59:25 -0300

    Add files via upload

---
+++ aula2.1.py
@@ -0,0 +1,7 @@
+variavel1 = 5
+variavel2 = 10
+variavel3 = 143
+soma das variaveis = variavel1 + variavel2 + variavel3
+print(soma das variaveis)
+
+# = é como se fosse um recebe
---
+++ aula2.2.py
@@ -0,0 +1,7 @@
+nota_A =int(input("valor da nota"))
+nota_B =int(input("valor da nota"))
+nota_C =int(input("VALOR DA nota"))
+media = (nota_A + nota_B + nota_C) /3
+print(media)
+
+
